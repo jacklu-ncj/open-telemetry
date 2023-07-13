a51a06bceee3fa158faecb81a85264a809d10717
jacklu-ncj <jack.lu@nocodejapan.org>
2023-07-12 19:53:23 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # open-telemetry
+
 An investigation project to do code instrumentation.
+
+Prerequsite:
+
+1. Run "docker run --rm -d -p 9411:9411 --name zikin openzipkin/zipkin" to install and run zipkin on your local docker. Zipkin is the telemetry data backend to help show the data.
+
+Steps:
+
+1. npm install
+2. npx ts-node --require ./tracing.ts app.ts
+
+Then hit the http://localhost:8080/rolldice a few times and go to the
+http://localhost:9411/zipkin/?lookback=15m&endTs=1689216670445&limit=10 and hit "run query" button and you should see the data.
+
+Have fun.
---
+++ tracing.ts
@@ -0,0 +1,20 @@
+/*tracing.ts*/
+/*instrumentation.ts*/
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import {
+  PeriodicExportingMetricReader,
+  ConsoleMetricExporter,
+} from "@opentelemetry/sdk-metrics";
+import { ZipkinExporter } from "@opentelemetry/exporter-zipkin";
+
+const sdk = new NodeSDK({
+  traceExporter: new ZipkinExporter({ serviceName: "rolldice" }),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new ConsoleMetricExporter(),
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk.start();
